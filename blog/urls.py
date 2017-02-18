2fa91d31d043fcadb19cdab6409c07e2dd960f83
honey <haney.sachdeva@gmail.com>
2017-02-18 19:23:45 +0530

    Added Login

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.contrib import admin
+import django.contrib.auth.views  as auth_views	
 from . import views
 
 urlpatterns = [
@@ -6,4 +8,6 @@ urlpatterns = [
     url(r'^post.(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+	url(r'^login/$', auth_views.login, name='login'),
+	url(r'^logout/$',auth_views.logout, name = 'logout'),
 ]
